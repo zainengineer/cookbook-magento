53f92059bd23dd543a3491a29d258cb2e111788e
Zain ul abidin <zain@aligent.com.au>
2015-07-23 18:16:23 +0930

    epel upgrade

---
+++ recipes/_epel_update.rb
@@ -0,0 +1,10 @@
+#Without it I cannot install php-mcrypt
+#http://stackoverflow.com/questions/17109818/install-php-mcrypt-on-centos-6
+case node['platform_family']
+when 'rhel', 'fedora'
+    if node['platform_version'].to_f < 6.5
+        execute 'upgrade_epl' do
+          command 'wget http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm && rpm -ivh epel-release-6-8.noarch.rpm'
+        end
+    end
+end
