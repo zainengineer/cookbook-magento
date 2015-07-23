c6e2cabfed3be06b31960d50821167b0305143c7
Zain ul abidin <zain@aligent.com.au>
2015-07-23 17:48:55 +0930

    apache php-fpm fix

--- recipes/_web_apache2.rb
+++ recipes/_web_apache2.rb
@@ -11,7 +11,28 @@ include_recipe 'apache2'
 Magento.create_ssl_cert(File.join(node[:apache][:dir], 'ssl'),
                         node[:magento][:domain], node[:magento][:cert_name])
 
+php_fpm_pools = node['php-fpm']['pools']
+default_listen = node[:magento][:nginx][:default_listen]
+php_fpm_pools.each do |name, config|
+  next if (name != "magento")
+  next if config.has_key?"enable" && !config['enable']
+  next if !config.has_key?"listen"
+  default_listen = config.listen
+end
+
+cgiDoc = File.join(node['apache']['docroot_dir'], 'php5.external')
+
+cgiHost = ' -host ' + default_listen
+
+
 %w(default ssl).each do |site|
+  if site == 'ssl'
+    cgissl =  '_ssl'
+  else
+    cgissl = ''
+  end
+  FastCgiExternalServer = cgiDoc + cgissl + cgiHost
+
   web_app "#{site}" do
     template 'apache2-site.conf.erb'
     docroot node[:magento][:dir]
@@ -20,6 +41,7 @@ Magento.create_ssl_cert(File.join(node[:apache][:dir], 'ssl'),
     ssl true if site == 'ssl'
     ssl_cert File.join(node[:apache][:dir], 'ssl', node[:magento][:cert_name])
     ssl_key File.join(node[:apache][:dir], 'ssl', node[:magento][:cert_name])
+    fast_cgi_external_server FastCgiExternalServer
   end
 end
 
